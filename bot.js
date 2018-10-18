9a4b2779657e9e5ed132422def86fbd6fa373b36
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-18 20:19:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,6 +78,52 @@ client.on('ready', () => {
     }
 });
 
+
+let points = JSON.parse(fs.readFileSync('./fkk/fkkPTS.json', 'utf8'));  
+client.on('message', message => {  
+if (!points[message.author.id]) points[message.author.id] = {  
+    points: 0,  
+  };  
+if (message.content.startsWith(prefix + 'فكك')) {  
+    if(!message.channel.guild) return message.refkky('**:x: هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));  
+const type = require('./fkk/fkk.json');  
+const item = type[Math.floor(Math.random() * type.length)];  
+const filter = response => {  
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());  
+};  
+message.channel.send('**لديك 15 ثانيه لتعرف اسم أي برنامج .**').then(msg => {  
+        
+msg.channel.sendFile(`${item.image}`).then(() => {  
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })  
+        .then((collected) => {  
+        message.channel.send(`${collected.first().author} ✅ **إجابة صحيحة .**`);  
+        console.log(`[Typing] ${collected.first().author} typed the word.`);  
+            let userData = points[message.author.id];  
+            userData.points++;  
+          })  
+          .catch(collected => {  
+            message.channel.send(`**تم الانتهاء من الوقت  حظ اوفر المره القادمه :stopwatch:  **`);  
+            console.log('[Typing] Error: No one type the word.');  
+          })  
+        })  
+    })  
+}  
+});  
+        
+client.on('message', message => {  
+if (message.content.startsWith(prefix + 'points')) {  
+    if(!message.channel.guild) return message.refkky('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));  
+    let userData = points[message.author.id];  
+    let embed = new Discord.RichEmbed()  
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)  
+    .setColor('#000000')  
+    .setDescription(`نقاطك: \`${userData.points}\``)  
+    message.channel.sendEmbed(embed)  
+  }      
+  fs.writeFile("./fkk/fkkPTS.json", JSON.stringify(points), (err) => {  
+    if (err) console.error(err)  
+  })  
+});  
 client.on('message', message => {
   if(message.content.startsWith(prefix + 'hrole')) {
       if(!message.member.hasPermission('MANAGE_ROLES')) return
